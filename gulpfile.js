45f98ab0fb558323598e2f5a2058fff05b8c0396
AleaJactaEst <kachan.vitaliy12@gmail.com>
2018-07-17 10:07:11 +0300

    test compress fonts and img

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var gzip = require('gulp-gzip');
 
 gulp.task('compress', function() {
-  gulp.src(['./dist/*.js', './dist/*.css'])
+  gulp.src(['./dist/*.js', './dist/*.css', './dist/*.jpg', './dist/*.woff', './dist/*.woff2'])
     .pipe(gzip())
     .pipe(gulp.dest('./dist'));
 });
